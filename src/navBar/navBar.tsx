f57731e5070e72f3edd78dbcdb0a6571440e6f2a
zclark0625 <zclark0625@gmail.com>
2019-05-24 15:14:53 -0400

    corrected singleSpa import

--- src/navBar/navBar.tsx
+++ src/navBar/navBar.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./navBar.scss";
-import {navigateToUrl} from "single-spa";
+import * as singleSpa from "single-spa";
 
 export default class NavBar extends React.Component {
     public render() {
         return (
             <div className={"root"}>
                 <ul>
-                    <li onClick={() => navigateToUrl("/app1")}>
+                    <li onClick={() => singleSpa.navigateToUrl("/app1")}>
                         App 1
                     </li>
-                    <li onClick={() => navigateToUrl("/app2")}>
+                    <li onClick={() => singleSpa.navigateToUrl("/app2")}>
                         App 2
                     </li>
                 </ul>
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,51 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/root-application/root-application.js',
+  mode: "production",
+  entry: "./src/root-application/root-application.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [{
         test: /\.jsx?$/,
-        exclude: [path.resolve(__dirname, 'node_modules')],
-        loader: 'babel-loader',
+        loader: "babel-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
+        loader: "ts-loader",
+        exclude: /node_modules/,
       },
       {
-        test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+        test: /\.s?css$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"],
       },
       {
-        test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"]
+        test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.ttf$/,
+        loader: "file-loader",
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.ts', '.tsx']
+    extensions: [".js", ".ts", ".tsx"],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Tracker',
-      filename: 'index.html',
-      template: './index.html.template',
-      favicon: './src/images/favicon.ico',
+      title: "Tracker",
+      filename: "index.html",
+      template: "./index.html.template",
+      favicon: "./src/images/favicon.ico",
       xhtml: true,
     }),
   ],
-  devtool: 'source-map',
+  devtool: "source-map",
   externals: [],
   devServer: {
-    historyApiFallback: true
-  }
+    historyApiFallback: true,
+  },
 };
